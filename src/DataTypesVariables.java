d445da247d11cd96c79b244177616052e27db093
Eljen Mae Augusto <86765650+EljenMae@users.noreply.github.com>
2025-12-29 18:40:26 +0800

    Inital commit

--- src/DataTypesVariables.java
+++ src/DataTypesVariables.java
@@ -8,9 +8,14 @@ public class DataTypesVariables {
         byte num3 = 50;
         short num4 = 65;
         char letter = 'C';
+        long numberLong = 9556655;
+        float numberFloat = 56002;
+        double numberDouble = 8554;
+
+        System.out.println("Primitive data");
 
         //getting the boolean
-        boolean newnum = num1 > num2; //calling the boolean
+        boolean newnum = num1 > num2; //the boolean newnnum here is the premitive
         System.out.println("Boolean: "+newnum);
 
         //getting the byte
@@ -24,5 +29,28 @@ public class DataTypesVariables {
         //getting the char
         char letterFinal = letter;
         System.out.println("Character: " + letterFinal);
+
+        //getting the int
+        int numFinal = num1;
+        System.out.println("Integer: " + numFinal);
+
+        //Getting the long
+        long numLong = numberLong;
+        System.out.println("Long: " + numLong);
+
+        //getting the float
+        float numfloat = numberFloat;
+        System.out.println("Float: " + numfloat);
+
+        //getting the double
+        double numDouble = numberDouble;
+        System.out.println("Double: " + numDouble);
+
+        //Objective types
+        System.out.println("Objective Types");
+
+        //Here is the set up
+
+
     }
 }
