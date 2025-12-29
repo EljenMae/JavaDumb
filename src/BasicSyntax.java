e99bee9bbc0fc381e19fc140cdaa8e1070790996
Eljen Mae Augusto <86765650+EljenMae@users.noreply.github.com>
2025-12-29 18:01:01 +0800

    Initial commit

--- src/BasicSyntax.java
+++ src/BasicSyntax.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
+import java.util.Scanner; //Input of the scanner this will make the input command here.
 
 public class BasicSyntax {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in); // This will be the input where I called the Scanner then name it scanner.
+        System.out.print("Enter your first number:");
+        int num1 = scanner.nextInt(); //This is a command to input the number here.
+        System.out.println("Your First Name: " + num1);
+    }
 }
--- src/DataTypesVariables.java
+++ src/DataTypesVariables.java
@@ -1,4 +1,28 @@
-package PACKAGE_NAME;
-
 public class DataTypesVariables {
+    public static void main(String[] args){
+        //This is the variables
+
+        //Primitive data types
+        int num1 = 50;
+        int num2 = 60;
+        byte num3 = 50;
+        short num4 = 65;
+        char letter = 'C';
+
+        //getting the boolean
+        boolean newnum = num1 > num2; //calling the boolean
+        System.out.println("Boolean: "+newnum);
+
+        //getting the byte
+        byte newnum1 = num3;
+        System.out.println("Byte: " + newnum1); //calling the byte
+
+        //getting the short
+        short newnum2 = num4;
+        System.out.println("Short: " + newnum2);
+
+        //getting the char
+        char letterFinal = letter;
+        System.out.println("Character: " + letterFinal);
+    }
 }
