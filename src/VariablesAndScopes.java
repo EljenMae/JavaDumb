07693102ce019b369f90a252fccee7ad10351c2a
Eljen Mae Augusto <86765650+EljenMae@users.noreply.github.com>
2025-12-30 18:11:54 +0800

    Inital commit

--- src/VariablesAndScopes.java
+++ src/VariablesAndScopes.java
@@ -1,5 +1,20 @@
 public class VariablesAndScopes {
-    public static void main(String[] args){
 
+    private Integer amount = 0; //This is for the command for the amount.
+
+    public static void main(String[] args) {
+        //Class Scope
+        VariablesAndScopes odj = new VariablesAndScopes(); // because it has this is because it the two class is not static.
+
+        odj.scope2Example();
+        odj.anotherExample();
     }
+    public void scope2Example(){
+        amount++;
+    }
+    public void anotherExample(){
+        Integer anotherAmount = amount + 4;
+        System.out.println("Amount: " + anotherAmount);
+    }
+
 }
