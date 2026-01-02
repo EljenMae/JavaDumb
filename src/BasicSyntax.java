2ed73011209faf01c948f28dba480590772e3fd8
Eljen Mae Augusto <86765650+EljenMae@users.noreply.github.com>
2026-01-02 15:31:54 +0800

    Initial commit

--- src/BasicSyntax.java
+++ src/BasicSyntax.java
@@ -2,9 +2,16 @@ import java.util.Scanner; //Input of the scanner this will make the input comman
 
 public class BasicSyntax {
     public static void main(String[] args) {
+        //practicing the input of number here.
         Scanner scanner = new Scanner(System.in); // This will be the input where I called the Scanner then name it scanner.
         System.out.print("Enter your first number:");
         int num1 = scanner.nextInt(); //This is a command to input the number here.
-        System.out.println("Your First Name: " + num1);
+        System.out.println("Your First number: " + num1);
+
+        //Practicing the inputs of string here
+        System.out.println("First Name:");
+        String name = scanner.nextLine(); //This is for the next line input.
+        System.out.println("Hello " + name + "!");
+        scanner.close(); //This part is to close the scanner part.
     }
 }
